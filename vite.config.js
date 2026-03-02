9efb93afa57512fa113fe0a46b29c4ebb9567633
Peter <chuams0228@gmail.com>
2026-03-03 02:29:54 +0800

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yudimamas/',
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
-        base: '/yudimamas/',
       },
     }),
   ],
