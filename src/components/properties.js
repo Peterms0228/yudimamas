587a8f99ac42384df0f01e70e38ebe705891d570
Peter <chuams0228@gmail.com>
2026-03-10 01:30:48 +0800

    change shop link

--- src/components/properties.js
+++ src/components/properties.js
@@ -1,6 +1,6 @@
 export const properties = {
     content: "This is a string"
-    ,shopeeLink:"https://l.facebook.com/l.php?u=https%3A%2F%2Fshopee.com.my%2Fyudimamascoconutcandy%3Ffbclid%3DIwZXh0bgNhZW0CMTAAYnJpZBExSUdEbURNR2Z2bFE4TkZ3N3NydGMGYXBwX2lkEDIyMjAzOTE3ODgyMDA4OTIAAR5LA-TbCWyNqQWg-T_5xVe0VeZR77Vmp1HLPziVtav64mec4CCobwS2ZTIX5w_aem_FCuB1Ze_vgchBs9zGlmCmQ&h=AT5KpDItJpr_ZncdiuAFg2d66xcrHuC8az8PIkN5d3qLEw1Na3HX6dYGqZApCgxijyiLHyq3-g_lePjbuycdr2QSGet3eJMQSfF388CND4kIxnZM-Bs_M8WIsTHlhbJNXXBFxXo5E03m4YLK5o0Y"
+    ,shopeeLink:"https://shopee.com.my/yudimamascoconutcandy?fbclid=IwY2xjawQSkTpleHRuA2FlbQIxMABicmlkETFJR0RtRE1HZnZsUThORnc3c3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHksD5NsJbI2pBaD5P_nFV7RV5lHvtWanUcs_OJW1q_riZ5zgIKhvBLZlMhfn_aem_FCuB1Ze_vgchBs9zGlmCmQ"
     ,phoneNumber: "0162241934"
     ,email: "yudimamas@gmail.com"
 };
